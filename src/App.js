350f256e5d534494dec76e0d966f9eb221f4a944
Alexander <alxndr+github@gmail.com>
2017-01-16 10:40:33 -0800

    testing...

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React...</h2>
-        </div>
-        <p className="App-intro">
+      <div className="app">
+        <p className="intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>

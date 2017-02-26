4d3aa71804629275596dd5277e81a19694ad947a
Alexander <alxndr+github@gmail.com>
2017-02-26 12:22:32 -0800

    indent the way eslint wants me to

--- src/reducer.js
+++ src/reducer.js
@@ -44,73 +44,73 @@ function reducer(state = loadState(), action) {
   const payload = action.payload;
   switch (action.type) {
 
-    case "ADD_SHOW": {
-      const showDate = action.payload;
-      const newShow = {
-        date: showDate,
-        tickets: [],
-      };
-      return {
-        ...state,
-        shows: {
-          ...state.shows,
-          [showDate]: newShow,
-        },
-      };
-    }
+  case "ADD_SHOW": {
+    const showDate = action.payload;
+    const newShow = {
+      date: showDate,
+      tickets: [],
+    };
+    return {
+      ...state,
+      shows: {
+        ...state.shows,
+        [showDate]: newShow,
+      },
+    };
+  }
 
-    case "ADD_TICKET": {
-      const theShow = state.shows[payload.date];
-      const ticket = newTicket(payload.name, payload.date);
-      return {
-        ...state,
-        tickets: {
-          ...state.tickets,
-          [ticket.id]: ticket,
+  case "ADD_TICKET": {
+    const theShow = state.shows[payload.date];
+    const ticket = newTicket(payload.name, payload.date);
+    return {
+      ...state,
+      tickets: {
+        ...state.tickets,
+        [ticket.id]: ticket,
+      },
+      shows: {
+        ...state.shows,
+        [payload.date]: {
+          ...theShow,
+          tickets: [
+            ...theShow.tickets,
+            ticket.id,
+          ],
         },
-        shows: {
-          ...state.shows,
-          [payload.date]: {
-            ...theShow,
-            tickets: [
-              ...theShow.tickets,
-              ticket.id,
-            ],
-          },
-        },
-      };
-    }
+      },
+    };
+  }
 
-    case "CHOOSE_SONG": {
-      const theTicket = state.tickets[`${payload.date}-${payload.playerName}`];
-      return {
-        ...state,
-        tickets: {
-          ...state.tickets,
-          [theTicket.id]: {
-            ...theTicket,
-            songs: [
-              ...theTicket.songs,
-              { title: payload.song }
-            ]
-          }
+  case "CHOOSE_SONG": {
+    const theTicket = state.tickets[`${payload.date}-${payload.playerName}`];
+    return {
+      ...state,
+      tickets: {
+        ...state.tickets,
+        [theTicket.id]: {
+          ...theTicket,
+          songs: [
+            ...theTicket.songs,
+            { title: payload.song }
+          ]
         }
-      };
-    }
+      }
+    };
+  }
 
-    case "REMOVE_SHOW": {
-      const show = state.shows[payload];
-      const ticketsForShow = Object.values(state.tickets).filter((ticket) => show.tickets.includes(ticket.id));
-      return {
-        ...state,
-        shows: objectWithoutKey(state.shows, show.date),
-        tickets: objectWithoutKeys(state.tickets, ticketsForShow.map((ticket) => ticket.id)),
-      };
-    }
+  case "REMOVE_SHOW": {
+    const show = state.shows[payload];
+    const ticketsForShow = Object.values(state.tickets).filter((ticket) => show.tickets.includes(ticket.id));
+    return {
+      ...state,
+      shows: objectWithoutKey(state.shows, show.date),
+      tickets: objectWithoutKeys(state.tickets, ticketsForShow.map((ticket) => ticket.id)),
+    };
+  }
 
-    default:
-      global.console.info("reducer saw unhandled action", state, action);
-      return state;
+  default:
+    global.console.info("reducer saw unhandled action", state, action);
+    return state;
   }
 }
 

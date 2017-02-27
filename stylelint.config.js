163126496fa669b2e106dbb56935f3212f54ae63
Alexander <alxndr+github@gmail.com>
2017-02-26 16:28:07 -0800

    css lint tweaks

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,6 @@
 const namingPattern = /^[a-z0-9]+(-[a-z0-9]+)*$/;
 
-module.exports = {
+module.exports = { // eslint-disable-line no-undef
   extends: "stylelint-config-standard",
   rules: {},
   namingPattern

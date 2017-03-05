808e654a6c756190493833e587c9d402ae47e14a
Alexander <alxndr+github@gmail.com>
2017-03-04 21:04:41 -0800

    Show can be a function

--- src/Show.js
+++ src/Show.js
@@ -1,35 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import moment from "moment";
 
 import PlayerTicket from "./PlayerTicket";
 
 import "./Show.css";
 
-class Show extends Component {
-  render() {
-    const location = this.props.location ? <p className="location">{this.props.location}</p> : false;
-    const dateFormatted = moment(this.props.date).format("MMM D, YYYY");
-    const dateContents = this.props.url ? <a href={this.props.url} target="_blank" rel="noopener noreferrer">{dateFormatted}</a> : dateFormatted;
-    return <div className="show">
-      <button onClick={this.props.removeShow} className="deleteShow">❌</button>
-      <button className="addPerson" onClick={() => this.props.addPerson(this.props.date)}>➕</button>
-      <p className="date">{dateContents}</p>
-      <button className="calculate" onClick={this.props.runTheNumbers}>💱</button>
-      {location}
-      <ul className="tickets">
-        {this.props.tickets.map((ticket) =>
-          <li key={ticket.id}>
-            <PlayerTicket
-              {...ticket}
-              chooseSong={this.props.chooseSong}
-              deleteTicket={this.props.removeTicket.bind(null, ticket.id)}
-              removeSong={this.props.removeSong}
-            />
-          </li>
-        )}
-      </ul>
-    </div>;
-  }
+function Show(props) {
+  const location = props.location ? <p className="location">{props.location}</p> : false;
+  const dateFormatted = moment(props.date).format("MMM D, YYYY");
+  const dateContents = props.url ? <a href={props.url} target="_blank" rel="noopener noreferrer">{dateFormatted}</a> : dateFormatted;
+  return <div className="show">
+    <button onClick={props.removeShow} className="deleteShow">❌</button>
+    <button className="addPerson" onClick={() => props.addPerson(props.date)}>➕</button>
+    <p className="date">{dateContents}</p>
+    <button className="calculate" onClick={props.runTheNumbers}>💱</button>
+    {location}
+    <ul className="tickets">
+      {props.tickets.map((ticket) =>
+        <li key={ticket.id}>
+          <PlayerTicket
+            {...ticket}
+            chooseSong={props.chooseSong}
+            deleteTicket={props.removeTicket.bind(null, ticket.id)}
+            removeSong={props.removeSong}
+          />
+        </li>
+      )}
+    </ul>
+  </div>;
 }
 
 export default Show;

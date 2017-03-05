6b4a13d5559a036e9ed75b1b105427184ed5a0b9
Alexander <alxndr+github@gmail.com>
2017-03-05 01:03:38 -0800

    alphabetize tests

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,5 +1,15 @@
 import * as utils from "./utils";
 
+describe("sanitizeString", () => {
+  const sanitizeString = utils.sanitizeString;
+
+  describe("with most non-alphanumeric stuff", () => {
+    it("makes them go away", () => {
+      expect(sanitizeString("`1234567890[]~!@#$%^&*(){}',.\"<>/=\\?+|-_;:")).toEqual("1234567890");
+    });
+  });
+});
+
 describe("slugify", () => {
   const slugify = utils.slugify;
 
@@ -8,16 +18,6 @@ describe("slugify", () => {
       expect(slugify(" foo bar")).toEqual("foo-bar");
       expect(slugify("%^& 123 baZZZ__!")).toEqual("123-bazzz");
       expect(slugify("\r\n%a__--!!'\"@b...")).toEqual("a-b");
-    });
-  });
-});
-
-describe("sanitizeString", () => {
-  const sanitizeString = utils.sanitizeString;
-
-  describe("with most non-alphanumeric stuff", () => {
-    it("makes them go away", () => {
-      expect(sanitizeString("`1234567890[]~!@#$%^&*(){}',.\"<>/=\\?+|-_;:")).toEqual("1234567890");
     });
   });
 });

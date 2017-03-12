2afb9a7688ae5dfedcfe3b7b2e2344da232326c2
Alexander <alxndr+github@gmail.com>
2017-03-11 17:47:33 -0800

    utils file now exports inline

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ const REGEX_ELIDABLE_CHARACTERS = /['.]/g;
 const REGEX_REPLACEABLE_CHARACTERS = /[^a-z0-9]+/g;
 const REGEX_STARTING_TRAILING_HYPHENS = /^-+|-+$/g;
 
-function arrayWithoutElement(array, element) {
+export function arrayWithoutElement(array, element) {
   if (!array.includes(element)) {
     console.warn("Element", element, "not found in array", array);
     return array;
@@ -21,28 +21,28 @@ function buildNewObject(newObj, [key, val]) {
   return newObj;
 }
 
-function extractJson(response) {
+export function extractJson(response) {
   return response.json();
 }
 
-function objectWithoutKey(obj, keyToRemove) {
+export function objectWithoutKey(obj, keyToRemove) {
   return Object.entries(obj)
     .filter(([key, _]) => key !== keyToRemove)
     .reduce(buildNewObject, {});
 }
 
-function objectWithoutKeys(obj, keysToRemove) {
+export function objectWithoutKeys(obj, keysToRemove) {
   return Object.entries(obj)
     .filter(([key, _]) => !keysToRemove.includes(key))
     .reduce(buildNewObject, {});
 }
 
 const REGEX_NON_ALPHANUMERIC = /[^a-z0-9]/g;
-function sanitizeString(string) {
+export function sanitizeString(string) {
   return string.toLowerCase().replace(REGEX_NON_ALPHANUMERIC, "");
 }
 
-function slugify(term) {
+export function slugify(term) {
   // lowercase, strips out some characters/punctuation, replaces everything non-alphanum with a hyphen
   if (!term || !term.trim) {
     return false;
@@ -54,16 +54,6 @@ function slugify(term) {
   return term.replace(REGEX_REPLACEABLE_CHARACTERS, "-").replace(REGEX_STARTING_TRAILING_HYPHENS, "");
 }
 
-function trimString(string = "") {
+export function trimString(string = "") {
   return string.trim();
-}
-
-export {
-  arrayWithoutElement,
-  extractJson,
-  objectWithoutKey,
-  objectWithoutKeys,
-  sanitizeString,
-  slugify,
-  trimString,
 }
